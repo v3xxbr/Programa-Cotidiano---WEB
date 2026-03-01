f8e412c9804aed0e8a9d22381ceeb7e9a4edd4c6
Naluluh <nalu16luna@gmail.com>
2026-03-01 07:11:28 -0300

    Adicionei o cálculo das faltas em Matemática e Global

--- contadordefaltas.js
+++ contadordefaltas.js
@@ -2,7 +2,7 @@
 
 //variáveis de Matemática
 let matTotal = 114;
-let faltasMat = 100;
+let faltasMat = 12;
 let porcFMat = 100;
 
 //variáveis de Português
@@ -18,10 +18,14 @@ function calculoDasFaltas(diasF, diasTotais){
     console.log("Então você faltou "+diasF+" dias.")
 
     porcFDaMatéria = (diasF/diasTotais)*100;
-    console.log("O total de faltas da matéria em questão é "+porcFDaMatéria.toFixed(2)+"%.");
+    console.log("O percentual total de faltas da matéria em questão é "+porcFDaMatéria.toFixed(2)+"%.");
 
     if(porcFDaMatéria>25){
         console.log("Você passou do limite em "+Math.abs(porcFDaMatéria-75).toFixed(2)+"% ("+("")+"de dias) ... Sinto muito...");
+    }
+
+     else{
+        console.log("Você está abaixo do limite em "+Math.abs(25-porcFDaMatéria).toFixed(2)+"% ("+("")+"de dias) ... Muito bem...");
     }
 
     return porcFDaMatéria;
@@ -30,6 +34,39 @@ function calculoDasFaltas(diasF, diasTotais){
 // caso...seja para a seleção de qual disciplina quer fazer a contagem das faltas 
 console.log("Selecione o código da matéria:\n1 - Matemática\n2 - Português\n3 - Global");
 let materia = 2;
+
+switch(materia){
+    case 1: 
+        porcFMat = calculoDasFaltas(faltasMat, matTotal);
+        break;
+    case 2:
+        porcFPort = calculoDasFaltas(faltasPort, portTotal);
+        break;
+    case 3:
+        let porcFGlobal = (100*(faltasMat+faltasPort))/aulasUteis;
+        console.log("A sua porcentagem global de faltas é de "+porcFGlobal.toFixed(2)+"%.")
+        break;
+}
+
+
+console.log("Selecione o código da matéria:\n1 - Matemática\n2 - Português\n3 - Global");
+materia = 1;
+
+switch(materia){
+    case 1: 
+        porcFMat = calculoDasFaltas(faltasMat, matTotal);
+        break;
+    case 2:
+        porcFPort = calculoDasFaltas(faltasPort, portTotal);
+        break;
+    case 3:
+        let porcFGlobal = (100*(faltasMat+faltasPort))/aulasUteis;
+        console.log("A sua porcentagem global de faltas é de "+porcFGlobal.toFixed(2)+"%.")
+        break;
+}
+
+console.log("Selecione o código da matéria:\n1 - Matemática\n2 - Português\n3 - Global");
+materia = 3;
 
 switch(materia){
     case 1: 
