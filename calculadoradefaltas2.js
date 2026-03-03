035ca47b103007e8f72c52a5e9a9cb100d8fe0b0
Naluluh <nalu16luna@gmail.com>
2026-03-03 15:42:57 -0300

    Código mais simples
    Refazendo o código sem a utilização de funções

--- calculadoradefaltas2.js
+++ calculadoradefaltas2.js
@@ -1,4 +1,4 @@
-//CALCULADORA DE FALTAS EM JAVASCRIPT (caso possa utilizar funções)
+//CALCULADORA DE FALTAS EM JAVASCRIPT (caso não possa utilizar funções)
 
 //variáveis
 let aulasTotal = 114;
@@ -6,6 +6,7 @@ let faltas = 100;
 let porcentagem = 100;
 let materia = 1
 let materia2 = 0
+let porcFGlobal = (100*(faltas + materia2))/aulasTotal;
 
 //função caso...seja para a seleção de qual disciplina quer fazer a contagem das faltas 
 console.log("Selecione o código da matéria:\n1 - Disciplina\n2 - Sair");
@@ -16,20 +17,20 @@ switch(materia){
         console.log("Então você faltou "+faltas+" dias.")
 
         porcFDaMatéria = (faltas/aulasTotal)*100;
-        console.log("O percentual total de faltas da matéria em questão é "+porcFDaMatéria.toFixed(2)+"%.");
+        console.log("O percentual total de faltas da matéria em questão é "+porcFDaMatéria.toFixed(2)+"%. ("+faltas.toFixed(0)+"/"+aulasTotal+ ")");
 
         limite = aulasTotal * 75 / 100
 
         if (faltas > limite) {
           dias = faltas - limite
-          console.log("Você passou do limite em "+porcFDaMatéria-75.toFixed(2)+"% ("+dias.toFixed(0)+" dias) ... Sinto muito...");
+          console.log("Você passou do limite em "+(porcFDaMatéria-75).toFixed(2)+"% ("+dias.toFixed(0)+" dias) ... Sinto muito...");
         
       } else if (faltas < limite) {
           dias = limite - faltas
-          console.log("Você está abaixo do limite em "+75-porcFDaMatéria.toFixed(2)+"% ("+dias.toFixed(0)+" dias) ... Muito bem...");
+          console.log("Você está abaixo do limite em "+(75-porcFDaMatéria).toFixed(2)+"% ("+dias.toFixed(0)+" dias) ... Muito bem...");
     }
-        let porcFGlobal = (100*(faltas+materia2))/aulasTotais;
-        console.log("A sua porcentagem global de faltas é de "+porcFGlobal.toFixed(2)+"%.")
+       
+        console.log("A sua porcentagem global de faltas é de "+(porcFGlobal).toFixed(2)+"%.")
         break;
     case 2:
         break;
